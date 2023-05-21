89d1e023a9a4562720bfc16daf3b55d13055ecb6
MISEONGKIM <rlaaltjd46@naver.com>
2023-05-21 14:00:58 +0900

    youtube api key발급까지

---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface IMainPageProps {}
+
+const MainPage: React.FunctionComponent<IMainPageProps> = () => {
+  return <div>MainPage</div>;
+};
+
+export default MainPage;
